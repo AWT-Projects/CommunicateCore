3950d034961c56c63bde6c5043d71c6a642b7d57
pcw1029 <pcw1029@gmail.com>
2025-04-16 13:32:37 +0900

    공통사용 헤더 분리 및 블럭다이어그램 업로드

--- uds-sensor-data-recv.c
+++ uds-sensor-data-recv.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <errno.h>
-#include <sys/select.h>
+
+
 #include "frame-format.h"
 #include "cmd-handler.h"
 #include "command.h"
